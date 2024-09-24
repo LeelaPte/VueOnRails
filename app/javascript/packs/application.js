c45fea4d6e975f38abb081ce9d439198db445905
Sebastian Zapata <sebastian@redsequoia.com>
2024-09-24 16:36:15 -0500

    [main] Adding VueJs

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,7 @@
+import * as Vue from 'vue'
+import App from '../app.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = Vue.createApp(App)
+  app.mount('#app')
+});
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  entry: './app/javascript/packs/application.js',
+  output: {
+    filename: '[name]-[hash].js',
+    path: path.resolve(__dirname, 'public/packs'),
+    publicPath: '/packs/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm-bundler.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/packs'),
+    compress: true,
+    port: 3035
+  }
+};
